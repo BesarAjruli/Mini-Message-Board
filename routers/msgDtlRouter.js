864d33044465e7807153917cee3dad9d15c2416b
BesarAjruli <aquila1912@gmail.com>
2024-12-21 22:10:49 +0100

    finished project

--- routers/msgDtlRouter.js
+++ routers/msgDtlRouter.js
@@ -2,8 +2,9 @@ const {Router} = require('express')
 
 const msgDtlRouter = Router()
 
-msgDtlRouter.get('/', (req, res) => {
-    res.render('messageDetail')
+msgDtlRouter.get('/:details', (req, res) => {
+    const {details} = req.params
+    res.render('messageDetail', {user:details})
 })
 
 module.exports = msgDtlRouter
